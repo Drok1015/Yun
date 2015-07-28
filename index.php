42824245a2bab9255bb26c7eb5b5732137815cf7
Drok1015 <271279841@qq.com>
2015-07-28 18:27:16 +0800

    主页修改

--- index.php
+++ index.php
@@ -9,35 +9,38 @@
 <!DOCTYPE>
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 </head>
 <body>
 
 <?php
 
-    $con = mysql_connect("55b486a68e646.gz.cdb.myqcloud.com:15757","root","ly881029");
+$con = mysql_connect("55b486a68e646.gz.cdb.myqcloud.com:15757", "root", "ly881029");
 
-    if (!$con)
-    {
-        die('Could not connect: ' . mysql_error());
-    }else{
-        echo "链接成功!"."<br />";
-    }
+if (!$con) {
+    die('Could not connect: ' . mysql_error());
+} else {
+    echo "链接成功!" . "<br />";
+}
 
-    mysql_select_db("test", $con);
+mysql_select_db("Shop", $con);
 
-    $result = mysql_query("select * from Android");
+    $result = mysql_query("select * from Shop_Admin");
 
     while($row = mysql_fetch_array($result))
     {
-        echo $row['uid'];
-        echo "<img src='upfiles/".$row['pic']."'>";
-        echo "<br />";
+        echo ' id:'.$row['id']."<br />";
+        echo ' username:'.$row['username']."<br />";
+        echo ' password:'.$row['password']."<br />";
+        echo ' email:'. $row['email']."<br />";
     }
 
-    mysql_close($con);
+//mysql_query("INSERT INTO Shop_Admin (username, password, email)
+//    VALUES ('drok2', 'drok10152', '271279841@qq.com2')");
 
-    include("common/menu.php");
+mysql_close($con);
+
+include("common/menu.php");
 ?>
 
 </body>
